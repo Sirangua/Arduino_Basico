307d2c59092a63bd9cf67f3f15215a143220c688
Sirangua <72269114+Sirangua@users.noreply.github.com>
2021-07-08 22:05:58 -0500

    función para mover en un sentido

--- Basic/Secuential_LED/Secuential_LED.ino
+++ Basic/Secuential_LED/Secuential_LED.ino
@@ -2,7 +2,7 @@
 const int red_led = 2;
 const int blue_led = 3;
 const int green_led = 4;
-const int pause = 1500;
+const int pause = 100;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,6 +14,22 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  left ();
+  right ();
+  left ();
+  right ();
+
+  delay (1000);
+
+  left (); 
+  left ();
+  left ();
+  right ();
+  right ();
+  right ();
+}
+
+void right () {
   digitalWrite (green_led,HIGH);
   digitalWrite (blue_led,LOW);
   digitalWrite (red_led,LOW);
@@ -30,3 +46,22 @@ void loop() {
   Serial.println("--> LED rojo encendido");
   delay (pause);
 }
+
+void left () {
+  digitalWrite (red_led,HIGH);
+  digitalWrite (green_led,LOW);
+  digitalWrite (blue_led,LOW);
+  Serial.println("--> LED rojo encendido");
+  delay (pause);
+  digitalWrite (blue_led,HIGH);
+  digitalWrite (green_led,LOW);
+  digitalWrite (red_led,LOW);
+  Serial.println("--> LED azul encendido");
+  delay (pause);
+  digitalWrite (green_led,HIGH);
+  digitalWrite (blue_led,LOW);
+  digitalWrite (red_led,LOW);
+  Serial.println("--> LED verde encendido");
+  delay (pause);
+}
+

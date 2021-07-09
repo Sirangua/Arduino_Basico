b41be52b1e32f166f84f5a1fd93c71ab27255034
Sirangua <72269114+Sirangua@users.noreply.github.com>
2021-07-08 23:39:30 -0500

    Programa que toma la lectura de un boton en el pin 5 y lo usamos para controlar un led

---
+++ Basic/Digital_input/Digital_input.ino
@@ -0,0 +1,23 @@
+//Define pin's name 
+const int led = 5;
+const int button  = 3;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode (button,INPUT);
+  pinMode (led,OUTPUT);
+}
+
+void loop() {
+  bool button_state = digitalRead (button);
+
+  if (button_state == false){
+    if (digitalRead(led) == LOW){
+      digitalWrite (led,HIGH);
+    } else {
+      digitalWrite (led, LOW);
+    }
+  }
+
+delay (400); 
+}

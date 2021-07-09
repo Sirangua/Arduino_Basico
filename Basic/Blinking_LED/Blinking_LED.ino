0140fbba5d96bca5cbe636967c0363ce39bd1104
Sirangua <72269114+Sirangua@users.noreply.github.com>
2021-07-08 21:47:36 -0500

    Definiendo nombre a cada puerto

--- Basic/Blinking_LED/Blinking_LED.ino
+++ Basic/Blinking_LED/Blinking_LED.ino
@@ -1,18 +1,22 @@
+//Define pin for led
+int blue_led = 3;
+int green_led = 5;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode (13,OUTPUT);
   pinMode (3,OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(9600); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite (13,HIGH);
-  digitalWrite (3,HIGH);
+  digitalWrite (green_led,HIGH);
+  digitalWrite (blue_led,HIGH);
   Serial.println ("---> Los leds están encendidos");
   delay (1000);
-  digitalWrite (13,LOW);
-  digitalWrite (3,LOW);
+  digitalWrite (green_led,LOW);
+  digitalWrite (blue_led,LOW);
   Serial.println ("---> Los leds están apagados");
   delay (1000);
 }
